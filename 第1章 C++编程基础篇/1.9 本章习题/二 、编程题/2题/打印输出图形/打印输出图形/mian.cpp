39aac290e39f92dbfccb735a5963e107acc1b138
sunzhong <sunzhong@coomaan.com>
2017-12-31 18:16:52 +0800

    提交“1.9 本章习题/二、编程题/2”

---
+++ 第1章 C++编程基础篇/1.9 本章习题/二 、编程题/2题/打印输出图形/打印输出图形/mian.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void main(void)
+{
+	cout << "* * * * * * * * * *"<< endl;
+	cout << "    * * * * * * * * * *" << endl;
+	cout << "* * * * * * * * * *" << endl;
+	cout << "    * * * * * * * * * *" << endl;
+	cout << "* * * * * * * * * *" << endl;
+	cout << "    * * * * * * * * * *" << endl;
+	cout << "* * * * * * * * * *" << endl;
+	while (1);
+}
