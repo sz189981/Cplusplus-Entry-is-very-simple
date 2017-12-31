4a40fd90631d37c7d606ca0120ac43c79b2689d6
sunzhong <sunzhong@coomaan.com>
2017-12-31 17:44:20 +0800

    提交第1个例子1.1

---
+++ 例子1.1 实现了最基本的打印功能/实现最基本的打印功能/实现最基本的打印功能/main.cpp
@@ -0,0 +1,19 @@
+/******************* Example 1.1.cpp **************************/
+#include <iostream>			//声明头文件
+using namespace std;		//声明命名空间
+
+void main(void)
+{
+	cout << "Welcome to C++" << endl;
+
+	while (1);
+}
+
+
+
+
+
+
+
+
+
