eb1c523c6a3c518b8476b688a5c09334d159f91d
sunzhong2 <1144186818@qq.com>
2018-01-11 08:02:21 +0800

    no message

---
+++ 第2章 开始C++开发之旅/例子2.4 使用输出操作符_20180110/如何使用输出操作符/如何使用输出操作符/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>		//使用控制符需要该头文件
+using namespace std;
+
+void main(void)
+{
+	int		ia = 12348;			//定义整形变量
+	double	d  = 12.3456789;	//定义浮点型变量
+	cout << "输出整数的不同进制和格式"<<endl;
+	cout << "ia的八进制："<<oct<<ia<<endl;
+	cout << "ia的十进制："<<dec<<ia<<endl;
+	cout << "ia的十六进制："<<hex<<ia<<endl;
+	
+
+
+	while(1);
+}
+
+
+
+
+
+
+
+
+
+
