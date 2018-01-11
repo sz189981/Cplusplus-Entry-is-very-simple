b3b02c9919bdfa9a84524e462cc838d2a1adab44
sunzhong <sunzhong@coomaan.com>
2018-01-11 21:56:07 +0800

    第2章/例子2.5 实现读取不同的数据类型并打印输出

--- 第2章 开始C++开发之旅/例子2.4 使用输出操作符_20180110/如何使用输出操作符/如何使用输出操作符/main.cpp
+++ 第2章 开始C++开发之旅/例子2.4 使用输出操作符_20180110/如何使用输出操作符/如何使用输出操作符/main.cpp
@@ -10,7 +10,23 @@ void main(void)
 	cout << "ia的八进制："<<oct<<ia<<endl;
 	cout << "ia的十进制："<<dec<<ia<<endl;
 	cout << "ia的十六进制："<<hex<<ia<<endl;
-	
+	//将十六进制中小写字母变为大写
+	cout << "ia设置大写标志输出：" << setiosflags(ios::uppercase) << ia << endl;
+	cout << dec;				//恢复默认输出进制格式
+	cout << "ia设置大写标志输出：" << setiosflags(ios::uppercase) << ia << endl;
+	cout << "ia输出位数：" << setw(10) << ia << endl;		//输出10个字节
+	//输出10个字节，不足用'*'填充
+	cout << "填充字符：" << setw(10) << setfill('*') << ia << endl;
+	cout << "显示ia的标志位：" << setiosflags(ios::showpos) << ia << endl;	//显示整数正负号
+	cout << resetiosflags(ios::showpos);					//对设置进行重置
+	cout << "------------------------------------------------" << endl;
+	cout << "输出浮点数不同格式" << endl;
+	cout << "d默认输出" << d << endl;
+	cout << "d设置输出精度为5：" << setprecision(5) << d << endl;	//输出精度控制
+	cout << setprecision(6);										//恢复默认精度
+	cout << "科学计数输出 d:" << setiosflags(ios::scientific) << d << endl;	//科学计数法输出
+
+
 
 
 	while(1);
